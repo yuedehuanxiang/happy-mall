474f786522c3b7ccca7a6db0826adbf671aaf48f
chenshuai <shuai.chen@wiz.ai>
2019-09-14 21:55:46 +0800

    react生命周期函数

--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,51 @@ import "./index.css";
 import "./index.scss";
 import "font-awesome/css/font-awesome.min.css";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "cs"
+    }
+    console.log("constructor")
+  }
+  componentWillMount() {
+    console.log("componentWillMount")
+  }
+  componentDidMount() {
+    console.log("componentDidMount")
+  }
+  componentWillReceiveProps() {
+    console.log("componentWillReceiveProps")
+  }
+  shouldComponentUpdate() {
+    return true
+  }
+  componentWillUpdate() {
+    console.log("componentWillUpdate")
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate")
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount")
+  }
+  handleClick() {
+    this.setState({
+      name: this.state.name + 'q'
+    })
+  }
+  render() {
+    console.log("render")
+    return (
+      <h1 onClick={() => {this.handleClick()}}>
+        hello world {this.state.name}
+      </h1>
+    )
+  }
+}
+
 ReactDOM.render(
-  <div>
-    <h1>hello, world11</h1>
-  </div>,
+  <App />,
   document.getElementById("app")
 );

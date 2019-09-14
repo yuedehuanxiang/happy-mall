764030388b2ff2d6c24d5d39aad291bd7285c98d
chenshuai <shuai.chen@wiz.ai>
2019-09-14 23:37:47 +0800

    react路由简介

--- src/app.jsx
+++ src/app.jsx
@@ -3,52 +3,61 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./index.scss";
 import "font-awesome/css/font-awesome.min.css";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+class ComponentA extends React.Component {
+  render() {
+    return (
+      <div>
+        componentA
+        <Switch>
+          <Route
+            exact
+            path={`${this.props.match.path}`}
+            render={() => {
+              return <div>当前是不带参数的A</div>;
+            }}
+          />
+          <Route
+            path={`${this.props.match.path}/:id`}
+            render={route => {
+              return <div>当前是带参数的A, 参数是{route.match.params.id}</div>;
+            }}
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+class ComponentB extends React.Component {
+  render() {
+    return <p>componentB</p>;
+  }
+}
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {
-      name: "cs"
-    }
-    console.log("constructor")
-  }
-  componentWillMount() {
-    console.log("componentWillMount")
-  }
-  componentDidMount() {
-    console.log("componentDidMount")
-  }
-  componentWillReceiveProps() {
-    console.log("componentWillReceiveProps")
-  }
-  shouldComponentUpdate() {
-    return true
-  }
-  componentWillUpdate() {
-    console.log("componentWillUpdate")
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate")
-  }
-  componentWillUnmount() {
-    console.log("componentWillUnmount")
-  }
-  handleClick() {
-    this.setState({
-      name: this.state.name + 'q'
-    })
+    super(props);
   }
   render() {
-    console.log("render")
-    return (
-      <h1 onClick={() => {this.handleClick()}}>
-        hello world {this.state.name}
-      </h1>
-    )
+    return <div>{this.props.children}</div>;
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App>
+      <div>
+        <Link to="/a">组件a</Link>
+        <br />
+        <Link to="/a/123">带参数的组件a</Link>
+        <br />
+        <Link to="/b">组件b</Link>
+      </div>
+      <Route path="/a" component={ComponentA} />
+      <Route path="/b" component={ComponentB} />
+    </App>
+  </Router>,
   document.getElementById("app")
 );
